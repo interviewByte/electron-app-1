87a9026e2dc0d2959d35c69447c1653838cad437
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2024-05-01 20:46:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Gtag } from 'angular-gtag';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'electron-app';
+
+  constructor(public gtag: Gtag){};
+
+  ngOnInit(): void {
+    this.gtag.pageview({ 
+      page_title: 'Lesson Feed',
+      page_path: '/lessons',
+      page_location: 'https://angularfirebase.com/lessons'
+    });  
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { GtagModule} from 'angular-gtag'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GtagModule.forRoot({ trackingId: 'G-DT1W5DPZR9', trackPageviews: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
